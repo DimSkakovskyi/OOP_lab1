106f2e3ab843f3768b49b1c5ee4237e74a951ea7
DimSkakovskyi <dimonskak@ukr.net>
2026-04-12 17:53:42 +0300

    Fixed configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,21 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const http = require('http');
-const Router = require('./routing/Router');
-const registerRoutes = require('./routing/routes');
-const pool = require('./config/db');
+// const http = require('http');
+// const Router = require('./routing/Router');
+// const registerRoutes = require('./routing/routes');
+// const pool = require('./config/db');
 
-const router = new Router();
-registerRoutes(router);
+// const router = new Router();
+// registerRoutes(router);
 
-pool.query('SELECT NOW()')
-  .then(() => console.log('DB connected'))
-  .catch((err) => console.error('DB connection error:', err));
+// pool.query('SELECT NOW()')
+//   .then(() => console.log('DB connected'))
+//   .catch((err) => console.error('DB connection error:', err));
 
-const server = http.createServer((req, res) => {
-  router.handle(req, res);
-});
+// const server = http.createServer((req, res) => {
+//   router.handle(req, res);
+// });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+// server.listen(3000, () => {
+//   console.log('Server running on port 3000');
+// });
